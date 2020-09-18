1441661a8ca4d19008fae25bda4181ac986ba37d
mjstokes86 <mjstokes1986@att.net>
2020-09-18 03:49:17 -0400

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+                <h1>Hello World! I am the Header!</h1>
+            </header>
+        </div>
+    )
+}
+
+
+
+export default Header
